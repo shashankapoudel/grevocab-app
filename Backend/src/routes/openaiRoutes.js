8658791e62ae4980799c05978d7e111124a312f1
shashankapoudel <poudelshashanka205@gmail.com>
2024-09-24 09:01:37 +0545

    some changes

--- Backend/src/routes/openaiRoutes.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express')
-// const { searchWordMeaning } = require('../controllers/openaiController')
-
-// const router = express.Router()
-
-// router.post('/search-word', searchWordMeaning)
-
-// module.exports = router;
